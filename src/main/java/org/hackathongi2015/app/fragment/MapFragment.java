aed41a0a0f1eb620f911c338b5dd6971b509708b
Ignacio Martín <ignacio.martin@udg.edu>
2015-03-28 21:46:24 +0100

    Fixed MapFragment

--- src/main/java/org/hackathongi2015/app/fragment/MapFragment.java
+++ src/main/java/org/hackathongi2015/app/fragment/MapFragment.java
@@ -1,23 +1,7 @@
 package org.hackathongi2015.app.fragment;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import org.hackathongi2015.R;
-
-public class MapFragment extends Fragment {
+public class MapFragment extends com.google.android.gms.maps.SupportMapFragment {
   public static final String ARG_OBJECT = "object";
 
-  @Override
-  public View onCreateView(LayoutInflater inflater,
-                           ViewGroup container, Bundle savedInstanceState) {
-    // The last two arguments ensure LayoutParams are inflated
-    // properly.
-    View rootView = inflater.inflate(
-        R.layout.map, container, false);
-    return rootView;
-  }
 }
