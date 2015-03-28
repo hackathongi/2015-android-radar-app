b8fa761c1268817b5f4ed578c438db2d7aea26f8
Ignacio Martín <ignacio.martin@udg.edu>
2015-03-28 17:03:15 +0100

    Changed to final URL. Added unsafe https access (ups)

--- src/main/java/org/hackathongi2015/App.java
+++ src/main/java/org/hackathongi2015/App.java
@@ -7,9 +7,16 @@ import com.google.gson.GsonBuilder;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.squareup.okhttp.OkHttpClient;
 import org.hackathongi2015.app.api.JobsService;
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
+
+import javax.net.ssl.*;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 
 public class App extends Application {
 
@@ -28,9 +35,12 @@ public class App extends Application {
         .setDateFormat("dd-MM-yyy HH:mm")
         .create();
 
+    OkHttpClient client =getUnsafeOkHttpClient();
+
     RestAdapter restAdapter = new RestAdapter.Builder()
         .setLogLevel(RestAdapter.LogLevel.FULL)
-        .setEndpoint("http://private-98213-hackathongi2015.apiary-mock.com")
+        .setEndpoint("https://api.wallyjobs.com")
+        .setClient(new OkClient(client))
         .setConverter(new GsonConverter(gson))
         .build();
 
@@ -61,5 +71,46 @@ public class App extends Application {
 
   public JobsService getREST() {
     return mREST;
+  }
+
+  private static OkHttpClient getUnsafeOkHttpClient() {
+    try {
+      // Create a trust manager that does not validate certificate chains
+      final TrustManager[] trustAllCerts = new TrustManager[] {
+          new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+              return null;
+            }
+          }
+      };
+
+      // Install the all-trusting trust manager
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+      OkHttpClient okHttpClient = new OkHttpClient();
+      okHttpClient.setSslSocketFactory(sslSocketFactory);
+      okHttpClient.setHostnameVerifier(new HostnameVerifier() {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      });
+
+      return okHttpClient;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
