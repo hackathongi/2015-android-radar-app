03b37abe3515897591e640d3167d3853d3bc6733
Ignacio Martín <ignacio.martin@udg.edu>
2015-03-28 17:03:52 +0100

    Added tabs to viewpager

--- src/main/java/org/hackathongi2015/app/activity/MainActivity.java
+++ src/main/java/org/hackathongi2015/app/activity/MainActivity.java
@@ -1,30 +1,62 @@
 package org.hackathongi2015.app.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import org.hackathongi2015.R;
 import org.hackathongi2015.app.adapter.CollectionPagerAdapter;
 
 public class MainActivity extends SherlockFragmentActivity {
 
-    CollectionPagerAdapter mDemoCollectionPagerAdapter;
-    ViewPager mViewPager;
+  CollectionPagerAdapter mDemoCollectionPagerAdapter;
+  ViewPager mViewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // setTheme(SampleList.THEME);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // setTheme(SampleList.THEME);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.act_main);
 
-        // ViewPager and its adapters use support library
-        // fragments, so use getSupportFragmentManager.
-        mDemoCollectionPagerAdapter =
-            new CollectionPagerAdapter(
-                getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    // ViewPager and its adapters use support library
+    // fragments, so use getSupportFragmentManager.
+    mDemoCollectionPagerAdapter = new CollectionPagerAdapter(getSupportFragmentManager());
+    mViewPager = (ViewPager) findViewById(R.id.pager);
+    mViewPager.setAdapter(mDemoCollectionPagerAdapter);
 
-    }
+    ActionBar abar = this.getSupportActionBar();
+    abar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+      public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+      }
+
+      public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // hide the given tab
+      }
+
+      public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // probably ignore this event
+      }
+    };
+
+    // Add the two tabs
+    abar.addTab(abar.newTab().setText("Job List").setTabListener(tabListener));
+    abar.addTab(abar.newTab().setText("Map").setTabListener(tabListener));
+    mViewPager.setOnPageChangeListener(
+
+        new ViewPager.SimpleOnPageChangeListener() {
+          @Override
+          public void onPageSelected(int position) {
+            // When swiping between pages, select the
+            // corresponding tab.
+            getSupportActionBar().setSelectedNavigationItem(position);
+          }
+        });
+
+
+  }
 }
