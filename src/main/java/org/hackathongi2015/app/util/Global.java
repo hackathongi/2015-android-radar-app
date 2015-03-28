a79c6a7cb74c60246c9b50466045d214f50e9e5c
Ignacio Martín <ignacio.martin@udg.edu>
2015-03-28 13:44:58 +0100

    Changed date formats

--- src/main/java/org/hackathongi2015/app/util/Global.java
+++ src/main/java/org/hackathongi2015/app/util/Global.java
@@ -14,10 +14,10 @@ import java.util.TimeZone;
 // Global variables (yes, I'm evil)
 public class Global {
     public static final String tz = TimeZone.getDefault().getDisplayName();
-    public static final SimpleDateFormat DATE_ONLY_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    public static final SimpleDateFormat DATE_ONLY_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
     public static final SimpleDateFormat TIME_ONLY_FORMAT = new SimpleDateFormat("HH::mm");
     public static final SimpleDateFormat TIME_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH::mm");
-    public static final SimpleDateFormat FULL_TIME_DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+    public static final SimpleDateFormat FULL_TIME_DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm::ss");
 
     // Resquest codes for startActivityForResult
     public static final int RQ_ADD_TASK = 1;
