abb0f1d7b2b20e06549de4e12427d7b58f32d0f8
Pere Garriga Roca <garriga.pere@gmail.com>
2015-03-28 16:46:33 +0100

    Get location at MainActivity.

--- src/main/java/org/hackathongi2015/app/activity/MainActivity.java
+++ src/main/java/org/hackathongi2015/app/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package org.hackathongi2015.app.activity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-
+import android.widget.Toast;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import org.hackathongi2015.R;
 import org.hackathongi2015.app.adapter.CollectionPagerAdapter;
@@ -21,10 +24,18 @@ public class MainActivity extends SherlockFragmentActivity {
         // ViewPager and its adapters use support library
         // fragments, so use getSupportFragmentManager.
         mDemoCollectionPagerAdapter =
-            new CollectionPagerAdapter(
-                getSupportFragmentManager());
+                new CollectionPagerAdapter(
+                        getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+
+        // Get location
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            Toast.makeText(this, location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
